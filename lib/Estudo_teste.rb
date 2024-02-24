df0bff2d33eb8e073ede218762f5408e885bd15c
lecunha23 <leandrojc23@gmail.com>
2024-02-24 15:18:46 -0300

    build: Estrutura teste

---
+++ lib/Estudo_teste.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Estudo_teste/version"
+
+module EstudoTeste
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Estudo_teste_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EstudoTeste do
+  it "has a version number" do
+    expect(EstudoTeste::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
