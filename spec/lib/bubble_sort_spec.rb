44b58661a0200780df444ff70b37c0a50fcc5b3a
lecunha23 <leandrojc23@gmail.com>
2024-03-01 07:05:39 -0300

    test: criando teste bubble sort

--- spec/lib/bubble_sort_spec.rb
+++ spec/lib/bubble_sort_spec.rb
@@ -4,8 +4,8 @@ require_relative '../../lib/bubble_sort'  # Supondo que sua função bubble_sort
 
 RSpec.describe "bubble_sort" do
   it "ordena corretamente uma lista desordenada" do
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    expected_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [64, 34, 25, 32, 22, 11, 90]
+    expected_arr = [11, 22, 25, 32, 34, 64, 90]
     sorted_arr = bubble_sort(input_arr)
     expect(sorted_arr).to eq(expected_arr)
   end
