fbe83f9ecdc44cf256d44fe47bda81891cfb9d40
lecunha23 <leandrojc23@gmail.com>
2024-03-05 22:34:11 -0300

    feat(): criando MinimumSum

--- lib/MinimumSum.rb
+++ lib/MinimumSum.rb
@@ -1,13 +1,25 @@
 
 class MinimumSum
-  def minimun_su(matrix)
+  def minimun_sun(matrix)
     #vai iniciar a variavel para armazenar a soma minima
     min_sum = 0
 
     matrix.each do |row|
       #adiciona o menor valor da linha a soma minima
       min_sum += row.min
-
     end
+
+    #retorna a soma minima
+    return min_sum
   end
 end
+
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+# cria uma instancia da classe
+min_sum_calculator = MinimumSum.new
+puts "Soma minima da matriz #{min_sum_calculator.minimun_sun(matrix)}"
