8e2e1098d9b828439fb6555d0d59c53d2eebfa30
lecunha23 <leandrojc23@gmail.com>
2024-03-07 21:15:18 -0300

    feat(): criando MinimumSum

--- lib/MinimumSum.rb
+++ lib/MinimumSum.rb
@@ -1,25 +1,6 @@
-
 class MinimumSum
-  def minimun_sun(matrix)
-    #vai iniciar a variavel para armazenar a soma minima
-    min_sum = matrix[0].sum
-
-    matrix.each do |row|
-      #adiciona o menor valor da linha a soma minima
-      min_sum += row.min
-    end
-
-    #retorna a soma minima
-    return min_sum
+  def minimum_sum(matrix)
+    min_val = matrix.flatten.min
+    return min_val * (matrix.length - 1)
   end
 end
-
-matrix = [
-  [5, 2, 3],
-  [9, 5, 6],
-  [2, 8, 9]
-]
-
-# cria uma instancia da classe
-min_sum_calculator = MinimumSum.new
-puts "Soma minima da matriz #{min_sum_calculator.minimun_sun(matrix)}"
--- spec/lib/MinimumSum_spec.rb
+++ spec/lib/MinimumSum_spec.rb
@@ -1,37 +1,17 @@
-require 'rspec'
+
 require_relative '../spec_helper'
 require_relative '../../lib/MinimumSum'
 
 RSpec.describe MinimumSum do
-  describe "#minimun_sum" do
+  describe "#minimum_sum" do
     it "calculates the minimum sum of the given matrix" do
       matrix = [
-        [5, 2, 3],
+        [5, 1, 3],
         [9, 5, 6],
         [2, 8, 9]
       ]
       min_sum_calculator = MinimumSum.new
-      expect(min_sum_calculator.minimun_sun(matrix)).to eq(9) # A soma mínima esperada é 9
-    end
-
-    it "handles a matrix with negative numbers correctly" do
-      matrix = [
-        [-5, -2, -3],
-        [-9, -5, -6],
-        [-2, -8, -9]
-      ]
-      min_sum_calculator = MinimumSum.new
-      expect(min_sum_calculator.minimun_sun(matrix)).to eq(-24) # A soma mínima esperada é -24
-    end
-
-    it "handles a matrix with zeros correctly" do
-      matrix = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-      ]
-      min_sum_calculator = MinimumSum.new
-      expect(min_sum_calculator.minimun_sun(matrix)).to eq(0) # A soma mínima esperada é 0
+      expect(min_sum_calculator.minimum_sum(matrix)).to eq(9) # A soma mínima esperada é 9
     end
   end
 end
