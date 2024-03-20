b8edbdf9ec9b74b7798f92638f4b0a85f930d9a2
lecunha23 <leandrojc23@gmail.com>
2024-03-19 21:51:31 -0300

    refactor(): teste ok

--- spec/lib/linear_search_spec.rb
+++ spec/lib/linear_search_spec.rb
@@ -1,13 +1,12 @@
 
 require 'rspec'
-ruby -v
 require_relative '../../lib/linear_search'
 
 RSpec.describe '#linear_search' do
   it ' retorna o indice da chave se ela estiver presente no array' do
     arr = [10, 50, 30, 70, 80, 20, 90, 40]
-    key = 30
-    expect(linear_search(arr, key)).to eq(2)
+    key = 10
+    expect(linear_search(arr, key)).to eq(0)
   end
 
   it 'retorna -1 se a chave nao estiver presente no array ' do
